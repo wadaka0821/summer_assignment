bdb89099947ec06251d4dcb5da60cca08d58aab9
wadaka0821 <wadaka0821@gmail.com>
2019-10-17 14:04:22 +0900

    finish 3,4

--- assignment1-10.py
+++ assignment1-10.py
@@ -1,3 +1,4 @@
+import re
 
 class Assignments:
 #-----------------------課題１------------------------------------
@@ -19,6 +20,29 @@ class Assignments:
             else:
                 print(i)
 #-----------------------課題３------------------------------------
+    def as3(self):
+        for i,j in enumerate(range(100,0,-1),1):
+            if i == 100:
+                print(j)
+            else:
+                print(j,end=", ")
+#-----------------------課題３------------------------------------
+    def as4(self):
+        while True:
+            num = input("整数を入力してください(終了する場合はe)\n")
+            find = re.search(r"[^\-0-9]",num)
+            if num == "e":
+                break
+            elif find:
+                print("正しい値を入力してください")
+                continue
+            else:
+                num = int(num)
+                if num % 2 == 0:
+                    print("偶数です")
+                else:
+                    print("奇数です")
+
 
 assignments = Assignments()
-assignments.as2()
+assignments.as4()
